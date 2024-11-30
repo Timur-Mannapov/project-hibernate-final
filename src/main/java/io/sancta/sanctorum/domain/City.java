d07f59e99e652b7d045730fa579527a3e65266fd
Timur-Mannapov <bla.blacarr@yandex.ru>
2024-11-30 15:11:36 +0300

    Изменил City, добавлены поля, аннотации

--- src/main/java/io/sancta/sanctorum/domain/City.java
+++ src/main/java/io/sancta/sanctorum/domain/City.java
@@ -1,4 +1,29 @@
 package io.sancta.sanctorum.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(schema = "world", name = "city")
 public class City {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String name;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    Country country;
+
+    String district;
+
+    Integer population;
+
 }
