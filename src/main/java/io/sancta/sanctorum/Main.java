70735547b36ee881726299d70f983fc7c6a9e34e
Timur-Mannapov <bla.blacarr@yandex.ru>
2024-11-30 15:13:45 +0300

    Добавлен GeoController в Main

--- src/main/java/io/sancta/sanctorum/Main.java
+++ src/main/java/io/sancta/sanctorum/Main.java
@@ -2,10 +2,8 @@ package io.sancta.sanctorum;
 
 public class Main {
 
-    public Main() {
-    }
-
     public static void main(String[] args) {
-        Main main = new Main();
+        GeoController geoController = new GeoController();
+        geoController.run();
     }
 }
