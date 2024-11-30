893ff8f759ead72c2a648e85a2f85185328cbd21
Timur-Mannapov <bla.blacarr@yandex.ru>
2024-11-30 15:12:34 +0300

    Изменил CountryLanguage, добавлены геттеры, добавлены аннотации

--- src/main/java/io/sancta/sanctorum/domain/CountryLanguage.java
+++ src/main/java/io/sancta/sanctorum/domain/CountryLanguage.java
@@ -1,4 +1,33 @@
 package io.sancta.sanctorum.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.Type;
+
+import java.math.BigDecimal;
+
+@Table(schema = "world", name = "country_language")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
 public class CountryLanguage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    Country country;
+
+    String language;
+
+    @Column(name = "is_official", columnDefinition = "BIT")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    Boolean isOfficial;
+
+    BigDecimal percentage;
 }
