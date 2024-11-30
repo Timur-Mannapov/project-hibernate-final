576edd92162ce1ad9773099df6d143bb8882143e
Timur-Mannapov <bla.blacarr@yandex.ru>
2024-11-30 15:11:56 +0300

    Изменил CityDAO, добавлены геттеры, добавлены аннотации

--- src/main/java/io/sancta/sanctorum/dao/CityDAO.java
+++ src/main/java/io/sancta/sanctorum/dao/CityDAO.java
@@ -1,4 +1,29 @@
 package io.sancta.sanctorum.dao;
 
+import io.sancta.sanctorum.domain.City;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+@FieldDefaults(level = AccessLevel.PRIVATE,makeFinal = true)
+@RequiredArgsConstructor()
 public class CityDAO {
+    SessionFactory sessionFactory;
+
+    public List<City> getItems(int offset, int limit){
+        Query<City> query = sessionFactory.getCurrentSession().createQuery("select c from City c", City.class);
+        query.setFirstResult(offset);
+        query.setMaxResults(limit);
+        return query.list();
+    }
+
+    public int getTotalCount(){
+        Query<Long> query = sessionFactory.getCurrentSession().createQuery("select count(c) from City c", Long.class);
+        return Math.toIntExact(query.getSingleResult());
+    }
 }
