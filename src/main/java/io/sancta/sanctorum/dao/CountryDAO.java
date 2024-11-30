240bf0a5b377b0cddd2fd499d00b39e8154dfd8b
Timur-Mannapov <bla.blacarr@yandex.ru>
2024-11-30 15:12:26 +0300

    Изменил CountryDAO, добавлен SessionFactory, добавлен геттер

--- src/main/java/io/sancta/sanctorum/dao/CountryDAO.java
+++ src/main/java/io/sancta/sanctorum/dao/CountryDAO.java
@@ -1,4 +1,24 @@
 package io.sancta.sanctorum.dao;
 
+import io.sancta.sanctorum.domain.Country;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
 public class CountryDAO {
+    SessionFactory sessionFactory;
+
+
+
+    public List<Country> getAll(){
+        String hql = "select c from Country c join fetch c.languages";
+        Query<Country> query = sessionFactory.getCurrentSession().createQuery(hql, Country.class);
+        return query.list();
+    }
 }
