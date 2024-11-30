5274fe7d50998c411bfaddf86478894a66b021ba
Timur-Mannapov <bla.blacarr@yandex.ru>
2024-11-30 16:21:14 +0300

    Обновил CityCountry, добавлены поля, аннотации Getter, Setter, Builder (паттерн Строитель)

--- src/main/java/io/sancta/sanctorum/redis/CityCountry.java
+++ src/main/java/io/sancta/sanctorum/redis/CityCountry.java
@@ -1,4 +1,29 @@
 package io.sancta.sanctorum.redis;
 
+import io.sancta.sanctorum.domain.Continent;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Builder
 public class CityCountry {
+    Integer id;
+    String name;
+    String district;
+    Integer population;
+    String countryCode;
+    String alternativeCountryCode;
+    String countryName;
+    Continent continent;
+    String countryRegion;
+    BigDecimal surfaceArea;
+    Integer countryPopulation;
+    Set<Language> languages;
+
+
 }
